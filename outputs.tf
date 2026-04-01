9aa7dec11c3747c23d7504c667dea795ef7a6058
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 15:49:38 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,42 @@
+#############################################
+# COMPARTMENT
+#############################################
+
 output "compartment_id" {
-  value = local.final_compartment_id
+  description = "Final Compartment OCID"
+  value       = local.final_compartment_id
 }
 
-output "backend_ips" {
-  value = local.backend_ips
+#############################################
+# NETWORK
+#############################################
+
+output "vcn_id" {
+  value = try(oci_core_vcn.vcn[0].id, var.vcn_id)
 }
 
-output "lb_id" {
-  value = try(oci_load_balancer_load_balancer.lb[0].id, null)
+output "subnet_id" {
+  value = try(oci_core_subnet.subnet[0].id, var.subnet_id)
+}
+
+#############################################
+# INSTANCE
+#############################################
+
+output "instance_private_ips" {
+  description = "Instance Private IPs"
+  value       = local.backend_ips
+}
+
+#############################################
+# LOAD BALANCER
+#############################################
+
+output "load_balancer_id" {
+  description = "Load Balancer OCID"
+  value       = try(oci_load_balancer_load_balancer.lb[0].id, null)
+}
+
+output "load_balancer_name" {
+  value = var.lb_name
 }
