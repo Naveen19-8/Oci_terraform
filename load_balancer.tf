9e1b49b88edbe4adce88826a3fab97cfe42103eb
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 11:24:36 +0530

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,8 +1,25 @@
 resource "oci_load_balancer_load_balancer" "lb" {
-  count          = var.create_lb ? 1 : 0
+  count = var.create_lb ? 1 : 0
+
   compartment_id = var.compartment_id
   display_name   = var.lb_name
   shape          = var.lb_shape
 
   subnet_ids = [var.subnet_id]
+
+  #############################################
+  # Optional: Flexible Shape Support
+  #############################################
+  dynamic "shape_details" {
+    for_each = var.lb_shape == "flexible" ? [1] : []
+    content {
+      minimum_bandwidth_in_mbps = 10
+      maximum_bandwidth_in_mbps = 100
+    }
+  }
+
+  #############################################
+  # Optional: Public / Private LB
+  #############################################
+  is_private = false
 }
