d57988b1e833cb30474dd4407b23c943986b7f90
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 11:22:11 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,18 @@
-output "lb_id" {
-  value = try(oci_load_balancer_load_balancer.lb[0].id, null)
+#############################################
+# OUTPUTS
+#############################################
+
+output "load_balancer_id" {
+  description = "Load Balancer OCID"
+  value       = try(oci_load_balancer_load_balancer.lb[0].id, null)
 }
 
-output "lb_name" {
-  value = var.lb_name
+output "load_balancer_name" {
+  description = "Load Balancer Name"
+  value       = var.lb_name
+}
+
+output "backend_ip" {
+  description = "Selected Backend IP"
+  value       = var.backend_ips
 }
