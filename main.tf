c63336eff435fb0a66af93f0c0db5d622f6fdf64
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 15:30:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,96 +1,85 @@
-
-
-#############################################
-# OCI Load Balancer - Main Entry
-#############################################
-
+#########################################
+# FINAL COMPARTMENT
+#########################################
 locals {
-  create_lb = var.create_lb
+  final_compartment_id = var.create_compartment ? oci_identity_compartment.comp[0].id : var.compartment_id
 }
 
-#############################################
-# Create Load Balancer
-#############################################
+#########################################
+# COMPARTMENT
+#########################################
+resource "oci_identity_compartment" "comp" {
+  count = var.create_compartment ? 1 : 0
 
-resource "oci_load_balancer_load_balancer" "lb" {
-  count = local.create_lb ? 1 : 0
-
+  name          = var.compartment_name
+  description   = "Created via Terraform"
   compartment_id = var.compartment_id
+}
+
+#########################################
+# NETWORK
+#########################################
+resource "oci_core_vcn" "vcn" {
+  count = var.create_network ? 1 : 0
+
+  cidr_block     = var.vcn_cidr
+  compartment_id = local.final_compartment_id
+}
+
+resource "oci_core_subnet" "subnet" {
+  count = var.create_network ? 1 : 0
+
+  cidr_block     = var.subnet_cidr
+  vcn_id         = oci_core_vcn.vcn[0].id
+  compartment_id = local.final_compartment_id
+}
+
+#########################################
+# COMPUTE
+#########################################
+resource "oci_core_instance" "app" {
+  count = var.create_instance ? var.instance_count : 0
+
+  display_name = "${var.instance_name}-${count.index}"
+  compartment_id = local.final_compartment_id
+  subnet_id = var.create_network ? oci_core_subnet.subnet[0].id : var.subnet_id
+
+  shape = "VM.Standard.E2.1.Micro"
+
+  source_details {
+    source_type = "image"
+    source_id   = "ocid1.image.oc1..." # replace
+  }
+}
+
+#########################################
+# COLLECT BACKEND IPS
+#########################################
+locals {
+  backend_ips = var.create_instance ?
+    [for i in oci_core_instance.app : i.private_ip] :
+    split(",", var.existing_instance_ips)
+}
+
+#########################################
+# LOAD BALANCER
+#########################################
+resource "oci_load_balancer_load_balancer" "lb" {
+  count = var.create_lb ? 1 : 0
+
+  compartment_id = local.final_compartment_id
   display_name   = var.lb_name
   shape          = var.lb_shape
 
-  subnet_ids = [var.subnet_id]
-
-  dynamic "shape_details" {
-    for_each = var.lb_shape == "flexible" ? [1] : []
-    content {
-      minimum_bandwidth_in_mbps = 10
-      maximum_bandwidth_in_mbps = 100
-    }
-  }
+  subnet_ids = [var.create_network ? oci_core_subnet.subnet[0].id : var.subnet_id]
 }
-
-#############################################
-# Backend Set
-#############################################
-
-resource "oci_load_balancer_backend_set" "backend_set" {
-  count            = local.create_lb ? 1 : 0
-  name             = "backendset1"
-  load_balancer_id = oci_load_balancer_load_balancer.lb[0].id
-  policy           = "ROUND_ROBIN"
-
-  health_checker {
-    protocol = "HTTP"
-    port     = 80
-    url_path = "/"
-  }
-}
-
-#############################################
-# Backend (single)
-#############################################
 
 resource "oci_load_balancer_backend" "backend" {
-  count = local.create_lb ? 1 : 0
+  count = var.create_lb ? length(local.backend_ips) : 0
 
   load_balancer_id = oci_load_balancer_load_balancer.lb[0].id
-  backendset_name  = oci_load_balancer_backend_set.backend_set[0].name
+  backendset_name  = "backendset1"
 
-  ip_address = var.backend_ips
+  ip_address = local.backend_ips[count.index]
   port       = 80
-}
-
-#############################################
-# Listener
-#############################################
-
-resource "oci_load_balancer_listener" "listener" {
-  count = local.create_lb ? 1 : 0
-
-  load_balancer_id         = oci_load_balancer_load_balancer.lb[0].id
-  name                     = "http_listener"
-  default_backend_set_name = oci_load_balancer_backend_set.backend_set[0].name
-  port                     = 80
-  protocol                 = "HTTP"
-}
-
-resource "oci_identity_compartment" "compartment" {
-  name          = var.compartment_name
-  description   = "Created via Terraform"
-  compartment_id = var.tenancy_ocid
-}
-
-resource "oci_identity_compartment" "new" {
-  count = var.create_compartment ? 1 : 0
-
-  name           = var.compartment_name
-  description    = "Created via Terraform"
-  compartment_id = var.tenancy_ocid
-}
-
-locals {
-  final_compartment_id = var.create_compartment
-    ? oci_identity_compartment.new[0].id
-    : var.compartment_id
 }
