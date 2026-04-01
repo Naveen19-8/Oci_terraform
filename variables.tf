a6aa2be72cd319cc1da97922445133ed613dbaf0
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 15:46:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,34 @@
+#########################################
+# COMPARTMENT
+#########################################
 variable "create_compartment" { type = bool }
 variable "compartment_name" { type = string }
 variable "compartment_id" { type = string }
 
+#########################################
+# NETWORK
+#########################################
 variable "create_network" { type = bool }
 variable "vcn_cidr" { type = string }
 variable "subnet_cidr" { type = string }
 variable "vcn_id" { type = string }
 variable "subnet_id" { type = string }
 
+#########################################
+# INSTANCE
+#########################################
 variable "create_instance" { type = bool }
 variable "instance_name" { type = string }
 variable "instance_count" { type = number }
 variable "existing_instance_ips" { type = string }
 
+#########################################
+# LOAD BALANCER
+#########################################
 variable "create_lb" { type = bool }
 variable "lb_name" { type = string }
 variable "lb_shape" { type = string }
+
 variable "is_private_lb" {
   type    = bool
   default = false
