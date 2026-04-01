1b1c364bd9ed5790a73574ee954caaefb0dc1d2d
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 15:49:14 +0530

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,36 +1,42 @@
-resource "oci_load_balancer_load_balancer" "lb" {
-  count = var.create_lb ? 1 : 0
+#############################################
+# COMPARTMENT
+#############################################
 
-  #############################################
-  # Compartment (dynamic)
-  #############################################
-  compartment_id = local.final_compartment_id
+output "compartment_id" {
+  description = "Final Compartment OCID"
+  value       = local.final_compartment_id
+}
 
-  display_name = var.lb_name
-  shape        = var.lb_shape
+#############################################
+# NETWORK
+#############################################
 
-  #############################################
-  # Subnet (Create OR Existing)
-  #############################################
-  subnet_ids = [
-    var.create_network
-    ? oci_core_subnet.subnet[0].id
-    : var.subnet_id
-  ]
+output "vcn_id" {
+  value = try(oci_core_vcn.vcn[0].id, var.vcn_id)
+}
 
-  #############################################
-  # Flexible Shape Support
-  #############################################
-  dynamic "shape_details" {
-    for_each = var.lb_shape == "flexible" ? [1] : []
-    content {
-      minimum_bandwidth_in_mbps = 10
-      maximum_bandwidth_in_mbps = 100
-    }
-  }
+output "subnet_id" {
+  value = try(oci_core_subnet.subnet[0].id, var.subnet_id)
+}
 
-  #############################################
-  # Public / Private LB
-  #############################################
-  is_private = false
+#############################################
+# INSTANCE
+#############################################
+
+output "instance_private_ips" {
+  description = "Instance Private IPs"
+  value       = local.backend_ips
+}
+
+#############################################
+# LOAD BALANCER
+#############################################
+
+output "load_balancer_id" {
+  description = "Load Balancer OCID"
+  value       = try(oci_load_balancer_load_balancer.lb[0].id, null)
+}
+
+output "load_balancer_name" {
+  value = var.lb_name
 }
