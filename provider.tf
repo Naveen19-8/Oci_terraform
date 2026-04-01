bcc0b5b8e0cfe8756044447dc6db7b4008576409
Naveen19-8 <naveen.christopher@excelenciaconsulting.com>
2026-04-01 15:35:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 provider "oci" {
-  region = var.region
-}
-variable "region" {
-  default = "ap-mumbai-1"
+  region              = var.region
+  tenancy_ocid        = var.tenancy_ocid
+  user_ocid           = var.user_ocid
+  fingerprint         = var.fingerprint
+  private_key_path    = var.private_key_path
 }
